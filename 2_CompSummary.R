ebab7ab441bd63b6e109ff2eae347e39d8774395
root <root@ip-172-31-26-204.us-west-2.compute.internal>
2016-12-20 22:03:57 +0000

    fixing aggregation issues

--- 2_CompSummary.R
+++ 2_CompSummary.R
@@ -80,69 +80,69 @@ kpis1 <- rbind(strip[c(1:3),])
 
 for (i in 1:8) {kpis1[,i] <- as.character(kpis1[,i])}
 
-kpis2 <- rbind(# Water appearance, smell, etc.
-  strip[4,] / strip[2,],
-  strip[5,] / strip[2,],
-  strip[6,] / strip[2,],
-  strip[7,] / strip[2,])
+# Water appearance, smell, etc.
+kpis2 <- rbind(strip[4,] / strip[2,],
+               strip[5,] / strip[2,],
+               strip[6,] / strip[2,],
+               strip[7,] / strip[2,])
 
 for (i in 1:8) {kpis2[,i] <- percent(kpis2[,i])}
 
-kpis3 <- rbind(# Health outcomes
-  strip[8,] / strip[2,],
-  strip[9,] / strip[2,],
-  strip[10,] / strip[2,],
-  strip[11,] / strip[2,],
-  strip[12,] / strip[2,],
-  strip[13,] / strip[2,],
-  strip[14,] / strip[2,],
-  strip[15,] / strip[2,])
+# Health outcomes
+kpis3 <- rbind(strip[8,] / strip[2,],
+               strip[9,] / strip[2,],
+               strip[10,] / strip[2,],
+               strip[11,] / strip[2,],
+               strip[12,] / strip[2,],
+               strip[13,] / strip[2,],
+               strip[14,] / strip[2,],
+               strip[15,] / strip[2,])
 
 for (i in 1:8) {kpis3[,i] <- percent(kpis3[,i])}
 
-kpis4 <- rbind(# Fetch Time, reordered to make sense
-  strip[19,] / strip[2,],
-  strip[20,] / strip[2,],
-  strip[22,] / strip[2,],
-  strip[21,] / strip[2,],
-  strip[23,] / strip[2,])
+# Fetch Time, reordered to make sense
+kpis4 <- rbind(strip[19,] / strip[2,],
+               strip[20,] / strip[2,],
+               strip[22,] / strip[2,],
+               strip[21,] / strip[2,],
+               strip[23,] / strip[2,])
 
 for (i in 1:8) {kpis4[,i] <- percent(kpis4[,i])}
 
-kpis5 <- rbind(# Sanitation Facilities
-  strip[25,] / strip[2,],
-  strip[26,] / strip[2,],
-  strip[27,] / strip[2,],
-  strip[28,] / strip[2,],
-  strip[29,] / strip[2,])
+# Sanitation Facilities
+kpis5 <- rbind(strip[25,] / strip[2,],
+               strip[26,] / strip[2,],
+               strip[27,] / strip[2,],
+               strip[28,] / strip[2,],
+               strip[29,] / strip[2,])
 
 for (i in 1:8) {kpis5[,i] <- percent(kpis5[,i])}
 
-kpis6 <- rbind(# Stored Fees
-  round(strip[49,]/1000000, 2))
+# Stored Fees
+kpis6 <- rbind(round(strip[49,]/1000000, 2))
 
 for (i in 1:8) {kpis6[,i] <- as.character(kpis6[,i])}
 
-kpis7 <- rbind(# Functionality - from water point and water quality surveys, separate
-  strip[46,] / strip[48,],
-  strip[51,] / strip[53,])
+# Functionality - from water point and water quality surveys, separate
+kpis7 <- rbind(strip[46,] / strip[48,],
+               strip[51,] / strip[53,])
 
 for (i in 1:8) {kpis7[,i] <- percent(kpis7[,i])}
 
-kpis8 <- rbind(# Water testing results
-  round(strip[54,] / strip[56,], 2),
-  round(strip[57,] / strip[59,], 2),
-  round(strip[60,] / strip[62,], 2),
-  round(strip[75,] / strip[77,], 2),
-  round(strip[78,] / strip[80,], 2))
+# Water testing results
+kpis8 <- rbind(round(strip[54,] / strip[56,], 2),
+               round(strip[57,] / strip[59,], 2),
+               round(strip[60,] / strip[62,], 2),
+               round(strip[75,] / strip[77,], 2),
+               round(strip[78,] / strip[80,], 2))
 
 for (i in 1:8) {kpis8[,i] <- as.character(kpis8[,i])}
 
-kpis9 <- rbind(# EColi risk buckets
-  strip[81,] / strip[1,],
-  strip[82,] / strip[1,],
-  strip[83,] / strip[1,],
-  strip[84,] / strip[1,])
+# EColi risk buckets
+kpis9 <- rbind(strip[81,] / strip[1,],
+               strip[82,] / strip[1,],
+               strip[83,] / strip[1,],
+               strip[84,] / strip[1,])
 
 for (i in 1:8) {kpis9[,i] <- percent(kpis9[,i])}
 
@@ -198,6 +198,122 @@ kpis_out <- cbind(c("Sites",
 colnames(kpis_out)[1] <- "Measure"
 
 write.csv(kpis_out, "Short_Summary.csv")
+
+rm(list = ls())
+
+####################################################################################################
+# Part 3: Metrics aggregated at site level before being rolled into other filters
+# Essentially, follows form of "site metric sum / site HH count"
+# site_agg is dependency
+# disregards metrics that counted NAs (anything ending in "_na")
+
+library(scales)
+
+sa <- read.csv("site_agg.csv", stringsAsFactors = F, colClasses = c(NA, NA, NA, NA, NA, "character", NA, NA, NA, NA, NA, NA, 
+                                                                    NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, 
+                                                                    NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, 
+                                                                    NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, 
+                                                                    NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, 
+                                                                    NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA))
+
+sa2 <- sa[,c(10:21, 24:35)]
+sa3 <- sa[,c(1:9)]
+for (i in 1:24) {
+sa3[,(i+9)] <- sa2[,i]/sa3[,8]
+colnames(sa3)[i+9] <- colnames(sa2)[i]
+}
+
+sa3$func_wp <- sa[,52]/sa[,54]
+sa3$func_wq <- sa[,57]/sa[,59]
+sa3$strd_fees <- sa[,55]
+sa3$ecoli_risk_l <- sa[,87]
+sa3$ecoli_risk_i <- sa[,88]
+sa3$ecoli_risk_h <- sa[,89]
+sa3$ecoli_risk_v <- sa[,90]
+
+for (i in 34:40) {
+sa3[is.na(sa3[, i]) == T, i] <- 0
+}
+
+write.csv(sa3, "site_agg_2.csv", row.names = F)
+
+rm(list = ls())
+
+####################################################################################################
+# Part 4: Rolls site_agg_2 into a summary for easy viewing
+
+sa <- read.csv("site_agg_2.csv", stringsAsFactors = F, colClasses = c(NA, NA, NA, NA, NA, "character", NA, NA, NA, NA, 
+                                                                      NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, 
+                                                                      NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, 
+                                                                      NA, NA, NA, NA, NA, NA, NA, NA, NA, NA))
+
+hldr <- data.frame(srvy_tp = rep(c("b", "m"), 4),
+                   newboth = c("00", "00", "01", "01", "10", "10", "11", "11"))
+
+for (i in 10:40) {
+  hldr <- merge(hldr, 
+                aggregate(sa[,i] ~ srvy_tp + newboth, sa, FUN = mean),
+                by = c("srvy_tp", "newboth"),
+                all.x = T)
+  colnames(hldr)[i-7] <- colnames(sa)[i]
+  
+  }
+
+
+hldr2 <- data.frame(srvy_tp = c("b", "m"),
+                    newboth = c("All", "All"))
+for (i in 10:40) {
+  hldr2 <- merge(hldr2, 
+                aggregate(sa[,i] ~ srvy_tp, sa, FUN = mean),
+                by = c("srvy_tp"),
+                all.x = T)
+  colnames(hldr2)[i-7] <- colnames(sa)[i]
+  
+}
+
+hldr3 <- rbind(hldr, 
+               hldr2)
+
+hldr4 <- hldr3[c(1, 5, 2, 6, 3, 7, 4, 8, 9, 10),]
+
+hldr5 <- data.frame(t(hldr4))
+hldr6 <- data.frame(row.names(hldr5),
+                    hldr5)
+
+colnames(hldr6) <- c("Measure", 
+                     "00 - B", "00 - M",
+                     "01 - B", "01 - M",
+                     "10 - B", "10 - M",
+                     "11 - B", "11 - M",
+                     "All - B", "All - M")
+
+hldr6 <- hldr6[c(-1, -2),]
+
+#####            START HERE            #####
+# TODO build in counts for sites, hh, ppl  #
+# join counts to hldr 6                    #
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
